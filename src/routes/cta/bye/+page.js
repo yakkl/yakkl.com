4cd106c6bb24db79a55c835631e9c6abc41bcaee
hanscj1 <cjones@cloudm2.com>
2023-06-02 16:52:43 -0400

    Removed cta bad link

--- src/routes/cta/bye/+page.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ params, url }) {
-  // let lang = url.searchParams.get('lang');
-  let userName = url.searchParams.get('userName');
-  return { userName };
-}
